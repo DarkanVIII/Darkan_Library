6ad55e6ee1344aa66b1b2336b1dfab0152d9d7f2
DarkanVIII <harkan.berger@sekai-vibe.de>
2023-10-16 22:11:19 +0200

    Improved Description

--- Generic State Machine/StateManager.cs
+++ Generic State Machine/StateManager.cs
@@ -8,11 +8,11 @@ namespace Darkan.StateMachine
 
     /// <summary>
     /// To use: <br/>
-    /// 1. Derive a State Manager from this and create a State Enum in it <br/>
-    /// 2. Derive States from <see cref="BaseState{TEnum, TManager}"/> <br/>
-    /// 3. Add AssetMenu Attribute to Base State and create a Scriptablöe Objects for each State
-    /// 4. Add State Objects to Disctionary of State Manager
-    /// Optional: Set Active State in Awake -> Starting State (Default is index 0 in Dictionary) <br/>
+    /// 1. Create and derive a State Manager from this and create a State Enum in it <br/>
+    /// 2. Create and derive States from <see cref="BaseState{TEnum, TManager}"/> <br/>
+    /// 3. Add AssetMenu Attribute to each Base State and create a Scriptable Objects of each State
+    /// 4. Add State Objects to Dictionary of State Manager
+    /// 5. Set Active State in Awake -> Starting State (Will pick randomly if not set to avoid Errors) <br/>
     /// To use Unity Messages like Update and OnTriggerEnter -> For Example add in State Manager : Update { ActiveState.Update }
     /// </summary>
     public abstract class StateManager<TEnum, TManager> : SerializedMonoBehaviour where TEnum : Enum where TManager : StateManager<TEnum, TManager>
