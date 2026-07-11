cfbfa882e5b3de6666c6bbfddf8c3c357a9b3f70
Harkan Berger <harkan.berger@outlook.com>
2026-07-11 02:41:03 +0200

    no message

--- Systems/StateMachines/Basic/StateMachine.cs
+++ Systems/StateMachines/Basic/StateMachine.cs
@@ -3,34 +3,22 @@ namespace Darkan.Systems.StateMachine.Basic
     using System;
     using System.Collections.Generic;
 
-    public abstract class StateMachine<T> : IDisposable where T : StateMachine<T>
+    public abstract class StateMachine<TMachine> : IDisposable where TMachine : StateMachine<TMachine>
     {
-        public event Action<State<T>> OnStateChanged;
-        public State<T> CurrentState { get; private set; }
+        public event Action<State<TMachine>> OnStateChanged;
+        public State<TMachine> CurrentState { get; private set; }
 
-        readonly Dictionary<Type, State<T>> _states = new();
+        readonly Dictionary<Type, State<TMachine>> _states = new();
 
-        public void ChangeState(State<T> to)
-        {
-            if (to == null) return;
-
-            CurrentState?.Exit();
-
-            CurrentState = to;
-            OnStateChanged?.Invoke(CurrentState);
-
-            CurrentState.Enter();
-        }
-
-        public void ChangeState<State>() where State : State<T>
+        public void ChangeState<TState>() where TState : State<TMachine>
         {
             CurrentState?.Exit();
 
-            if (!_states.TryGetValue(typeof(State), out State<T> to))
+            if (!_states.TryGetValue(typeof(TState), out State<TMachine> to))
             {
-                to = Activator.CreateInstance<State>();
-                _states[typeof(State)] = to;
-                to.Initialize((T)this);
+                to = Activator.CreateInstance<TState>();
+                _states[typeof(TState)] = to;
+                to.Initialize((TMachine)this);
             }
 
             CurrentState = to;
@@ -41,22 +29,26 @@ namespace Darkan.Systems.StateMachine.Basic
 
         public void Dispose()
         {
-            CurrentState?.Dispose();
+            foreach (var state in _states.Values)
+                state?.Dispose();
+
             OnStateChanged = null;
         }
     }
 
-    public abstract class State<T> : IDisposable where T : StateMachine<T>
+    public abstract class State<TMachine> : IDisposable where TMachine : StateMachine<TMachine>
     {
-        protected T _stateMachine { get; private set; }
+        protected TMachine _stateMachine { get; private set; }
 
-        public void Initialize(T stateMachine)
+        public void Initialize(TMachine stateMachine)
         {
             _stateMachine = stateMachine;
         }
 
         public abstract void Enter();
         public abstract void Exit();
+        public virtual void Update() { }
+        public virtual void FixedUpdate() { }
         public virtual void Dispose() { }
     }
 }
